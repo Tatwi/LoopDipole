0dae8a80041b1ff1bb3b43b3ffa8cf7e089b62c1
rob <rbassettjr@tpot.ca>
2015-01-26 00:54:32 -0500

    - Restructured variables (so they actually work now!) - Changing shapes using NumPad 1 - 4 - Shapes have unique handling characteristics - Turbo: Added top speed limit, time limit, and cooldown - Glide: Added a time limit, cooldown, and fixed lift offs - Reverse speed slower - Brake speed adjustable - Added 4th Shape (player/vehicle/Loop!) - Replaced Vehicle Demo landscape with giant open plane for testing

--- scripts.py
+++ scripts.py
@@ -31,142 +31,262 @@ from bge import constraints
 from bge import logic
 from bge import events
 from bge import render
-
 import math
 
-## Set specific vehicle characteristics ##
-'''
-Vehicle Demo Default Values
-wheelRadius = 0.5
-wheelBaseWide = 1.0
-wheelFrontOffset = 0.7
-wheelBackOffset = -1.8
-AttachHeightLocal = 0.2
-suspensionLength = 0.8
-influence = 0.02
-stiffness = 20.0
-damping = 2.0
-compression = 4.0
-friction = 10.0
-Stability = 0.05
-'''
-wheelRadius = 0.5
-wheelBaseWide = 1.0
-wheelFrontOffset = 0.7
-wheelBackOffset = -1.8
-AttachHeightLocal = 0.2
-suspensionLength = 0.8
-influence = 0.01
-stiffness = 20.0
-damping = 2.0
-compression = 4.0
-friction = 10.0
-Stability = 0.05
 
-## This is called from the car object
+## Set/Change the wheel stats in their own function
+## because there are a load of them!
+def setWheelStats():
+    ## Apply handling stats to the (invisible) wheels
+    vehicle = constraints.getVehicleConstraint(logic.car["cid"])
+    ## Grab base stats from "BaseStats" object
+    bstat = logic.scene.objects["BaseStats"]
+    ## set vehicle roll tendency ##
+    vehicle.setRollInfluence(bstat["influence"],0)
+    vehicle.setRollInfluence(bstat["influence"],1)
+    vehicle.setRollInfluence(bstat["influence"],2)
+    vehicle.setRollInfluence(bstat["influence"],3)
+    ## set vehicle suspension hardness ##
+    vehicle.setSuspensionStiffness(bstat["stiffness"],0)
+    vehicle.setSuspensionStiffness(bstat["stiffness"],1)
+    vehicle.setSuspensionStiffness(bstat["stiffness"],2)
+    vehicle.setSuspensionStiffness(bstat["stiffness"],3)
+    ## set vehicle suspension dampness ##
+    vehicle.setSuspensionDamping(bstat["damping"],0)
+    vehicle.setSuspensionDamping(bstat["damping"],1)
+    vehicle.setSuspensionDamping(bstat["damping"],2)
+    vehicle.setSuspensionDamping(bstat["damping"],3)
+    ## set vehicle suspension compression ratio ##
+    vehicle.setSuspensionCompression(bstat["compression"],0)
+    vehicle.setSuspensionCompression(bstat["compression"],1)
+    vehicle.setSuspensionCompression(bstat["compression"],2)
+    vehicle.setSuspensionCompression(bstat["compression"],3)
+    ## set vehicle tire friction ##
+    vehicle.setTyreFriction(bstat["friction"],0)
+    vehicle.setTyreFriction(bstat["friction"],1)
+    vehicle.setTyreFriction(bstat["friction"],2)
+    vehicle.setTyreFriction(bstat["friction"],3)
+
+
+# Reset stats when changing shapes
+def resetStats(): 
+    ## Max/Top Speed
+    logic.car.linVelocityMax = 40
+    
+    ## Default Shape
+    logic.car["activeShape"] = 0
+    
+    logic.car["accelNormal"] = -10.0
+    logic.car["accelTurbo"] = -14.0
+    logic.car["turboDur"] = 5.0
+    logic.car["brakeForce"] = 15.0
+    logic.car["steerAmount"] = 0.1
+    
+    ## Lift impulse or "jump" 0.0, Forward, Up
+    logic.car.actuators["Glide"].linV  = [ 0.0, 0.1, 0.25]
+    
+    logic.car["glidecooldown"] = 10
+    logic.car["glideBonusY"] = 0.2
+    logic.car["glideBonusZ"] = 0.2
+  
+    ## Base Stats
+    bstat = logic.scene.objects["BaseStats"]
+    bstat["influence"] = 0.01
+    bstat["stiffness"] = 20.0
+    bstat["damping"] = 2.0
+    bstat["compression"] = 4.0
+    bstat["friction"] = 10.0
+    bstat["Stability"] = 0.1
+
+
+## This is called from the object named "bare"
 ## is run once at the start of the game
 def carInit():
-        ## setup aliases for Blender API access ##
-        cont = logic.getCurrentController()
-        logic.scene = logic.getCurrentScene()
-        logic.car  = cont.owner
+    ## setup aliases for Blender API access ##
+    cont = logic.getCurrentController()
+    logic.scene = logic.getCurrentScene()
+    logic.car  = cont.owner
+    
+    ## Grab base stats from "BaseStats" object
+    bstat = logic.scene.objects["BaseStats"]
+    
+    ## Constants
+    wheelRadius = bstat["wheelRadius"]
+    wheelBaseWide = bstat["wheelBaseWide"]
+    wheelFrontOffset = bstat["wheelFrontOffset"]
+    wheelBackOffset = bstat["wheelBackOffset"]
+    AttachHeightLocal = bstat["AttachHeightLocal"]
+    suspensionLength = bstat["suspensionLength"]
 
-        ## setup general vehicle characteristics ##
-        wheelAttachDirLocal = [0,0,-1]
-        wheelAxleLocal = [-1,0,0]
+    ## setup general vehicle characteristics ##
+    wheelAttachDirLocal = [0,0,-1]
+    wheelAxleLocal = [-1,0,0]
 
-        ## setup vehicle physics ##
-        vehicle = constraints.createConstraint(logic.car.getPhysicsId(), 0, constraints.VEHICLE_CONSTRAINT)
-        logic.car["cid"] = vehicle.getConstraintId()
-        vehicle = constraints.getVehicleConstraint(logic.car["cid"])
+    ## setup vehicle physics ##
+    vehicle = constraints.createConstraint(logic.car.getPhysicsId(), 0, constraints.VEHICLE_CONSTRAINT)
+    logic.car["cid"] = vehicle.getConstraintId()
+    vehicle = constraints.getVehicleConstraint(logic.car["cid"])
 
-        ## initialize temporary variables ##
-        logic.car["dS"] = 0.0
+    ## initialize temporary variables ##
+    logic.car["dS"] = 0.0
 
-        ## attached wheel based on actuator name ##
-        wheel0 = logic.scene.objects["Wheel0"]
-        wheelAttachPosLocal = [wheelBaseWide ,wheelFrontOffset, AttachHeightLocal]
-        vehicle.addWheel(wheel0,wheelAttachPosLocal,wheelAttachDirLocal,wheelAxleLocal,suspensionLength,wheelRadius,1)
+    ## attached wheel based on actuator name ##
+    wheel0 = logic.scene.objects["Wheel0"]
+    wheelAttachPosLocal = [wheelBaseWide ,wheelFrontOffset, AttachHeightLocal]
+    vehicle.addWheel(wheel0,wheelAttachPosLocal,wheelAttachDirLocal,wheelAxleLocal,suspensionLength,wheelRadius,1)
 
-        wheel1 = logic.scene.objects["Wheel1"]
-        wheelAttachPosLocal = [-wheelBaseWide ,wheelFrontOffset, AttachHeightLocal]
-        vehicle.addWheel(wheel1,wheelAttachPosLocal,wheelAttachDirLocal,wheelAxleLocal,suspensionLength,wheelRadius,1)
+    wheel1 = logic.scene.objects["Wheel1"]
+    wheelAttachPosLocal = [-wheelBaseWide ,wheelFrontOffset, AttachHeightLocal]
+    vehicle.addWheel(wheel1,wheelAttachPosLocal,wheelAttachDirLocal,wheelAxleLocal,suspensionLength,wheelRadius,1)
 
-        wheel2 = logic.scene.objects["Wheel2"]
-        wheelAttachPosLocal = [wheelBaseWide ,wheelBackOffset, AttachHeightLocal]
-        vehicle.addWheel(wheel2,wheelAttachPosLocal,wheelAttachDirLocal,wheelAxleLocal,suspensionLength,wheelRadius,0)
+    wheel2 = logic.scene.objects["Wheel2"]
+    wheelAttachPosLocal = [wheelBaseWide ,wheelBackOffset, AttachHeightLocal]
+    vehicle.addWheel(wheel2,wheelAttachPosLocal,wheelAttachDirLocal,wheelAxleLocal,suspensionLength,wheelRadius,0)
 
-        wheel3 = logic.scene.objects["Wheel3"]
-        wheelAttachPosLocal = [-wheelBaseWide ,wheelBackOffset, AttachHeightLocal]
-        vehicle.addWheel(wheel3,wheelAttachPosLocal,wheelAttachDirLocal,wheelAxleLocal,suspensionLength,wheelRadius,0)
+    wheel3 = logic.scene.objects["Wheel3"]
+    wheelAttachPosLocal = [-wheelBaseWide ,wheelBackOffset, AttachHeightLocal]
+    vehicle.addWheel(wheel3,wheelAttachPosLocal,wheelAttachDirLocal,wheelAxleLocal,suspensionLength,wheelRadius,0)
 
-        ## set vehicle roll tendency ##
-        vehicle.setRollInfluence(influence,0)
-        vehicle.setRollInfluence(influence,1)
-        vehicle.setRollInfluence(influence,2)
-        vehicle.setRollInfluence(influence,3)
-
-        ## set vehicle suspension hardness ##
-        vehicle.setSuspensionStiffness(stiffness,0)
-        vehicle.setSuspensionStiffness(stiffness,1)
-        vehicle.setSuspensionStiffness(stiffness,2)
-        vehicle.setSuspensionStiffness(stiffness,3)
-
-        ## set vehicle suspension dampness ##
-        vehicle.setSuspensionDamping(damping,0)
-        vehicle.setSuspensionDamping(damping,1)
-        vehicle.setSuspensionDamping(damping,2)
-        vehicle.setSuspensionDamping(damping,3)
-
-        ## set vehicle suspension compression ratio ##
-        vehicle.setSuspensionCompression(compression,0)
-        vehicle.setSuspensionCompression(compression,1)
-        vehicle.setSuspensionCompression(compression,2)
-        vehicle.setSuspensionCompression(compression,3)
-
-        ## set vehicle tire friction ##
-        vehicle.setTyreFriction(friction,0)
-        vehicle.setTyreFriction(friction,1)
-        vehicle.setTyreFriction(friction,2)
-        vehicle.setTyreFriction(friction,3)
+    ## set the default values for anything that can be changed later
+    resetStats()
+    setWheelStats()
 
 
 ## called from main car object
-## is run once at the start of the game
 def carHandler():
+    vehicle = constraints.getVehicleConstraint(logic.car["cid"])
 
-        vehicle = constraints.getVehicleConstraint(logic.car["cid"])
+    ## calculate speed by using the back wheel rotation speed ##
+    S = vehicle.getWheelRotation(2)+vehicle.getWheelRotation(3)
+    logic.car["speed"] = (S - logic.car["dS"])*10.0
 
-        ## calculate speed by using the back wheel rotation speed ##
-        S = vehicle.getWheelRotation(2)+vehicle.getWheelRotation(3)
-        logic.car["speed"] = (S - logic.car["dS"])*10.0
+    ## apply engine force ##
+    vehicle.applyEngineForce(logic.car["force"],0)
+    vehicle.applyEngineForce(logic.car["force"],1)
+    vehicle.applyEngineForce(logic.car["force"],2)
+    vehicle.applyEngineForce(logic.car["force"],3)
 
-        ## apply engine force ##
-        vehicle.applyEngineForce(logic.car["force"],0)
-        vehicle.applyEngineForce(logic.car["force"],1)
-        vehicle.applyEngineForce(logic.car["force"],2)
-        vehicle.applyEngineForce(logic.car["force"],3)
+    ## calculate steering with varying sensitivity ##
+    if math.fabs(logic.car["speed"])<15.0: s = 2.0
+    elif math.fabs(logic.car["speed"])<28.0: s=1.5
+    elif math.fabs(logic.car["speed"])<40.0: s=1.0
+    else: s=0.5
 
-        ## calculate steering with varying sensitivity ##
-        if math.fabs(logic.car["speed"])<15.0: s = 2.0
-        elif math.fabs(logic.car["speed"])<28.0: s=1.5
-        elif math.fabs(logic.car["speed"])<40.0: s=1.0
-        else: s=0.5
+    ## steer front wheels
+    vehicle.setSteeringValue(logic.car["steer"]*s,0)
+    vehicle.setSteeringValue(logic.car["steer"]*s,1)
 
-        ## steer front wheels
-        vehicle.setSteeringValue(logic.car["steer"]*s,0)
-        vehicle.setSteeringValue(logic.car["steer"]*s,1)
+    ## slowly ease off gas and center steering ##
+    logic.car["steer"] *= 0.6
+    logic.car["force"] *= 0.9
 
-        ## slowly ease off gas and center steering ##
-        logic.car["steer"] *= 0.6
-        logic.car["force"] *= 0.9
+    ## align car to Z axis to prevent flipping ##
+    bstat = logic.scene.objects["BaseStats"]
+    logic.car.alignAxisToVect([0.0,0.0,1.0], 2, bstat["Stability"])
+    
+    ## store old values ##
+    logic.car["dS"] = S
 
-        ## align car to Z axis to prevent flipping ##
-        logic.car.alignAxisToVect([0.0,0.0,1.0], 2, Stability)
-        
-        ## store old values ##
-        logic.car["dS"] = S
 
+## Set all player shapes invisible
+def makeInvisible():
+    logic.scene.objects["Loop 1_proxy"].setVisible(False)
+    logic.scene.objects["Loop 2_proxy"].setVisible(False)
+    logic.scene.objects["Loop 3_proxy"].setVisible(False)
+    logic.scene.objects["Loop 4_proxy"].setVisible(False)
+
+
+## Change Shapes
+## Any stat not listed in an if block is set to the default value
+## I figured this make it easier to see the differences
+def changeShape(choice):
+    makeInvisible()
+    resetStats()
+    
+    ## Grab Base Stats to modify them
+    bstat = logic.scene.objects["BaseStats"]
+
+    if choice == 1:
+        ## Basic
+        ## Uses the default stats (Nimble, yet slow and easy to handle)
+        logic.scene.objects["Loop 1_proxy"].setVisible(True)
+    elif choice == 2:
+        ## Bomber
+        ## Higher top speed, slow turning, stable, does not take damage
+        logic.scene.objects["Loop 2_proxy"].setVisible(True)
+        logic.car["activeShape"] = 1
+        logic.car.linVelocityMax = 90
+        logic.car["glideBonusY"] = 0.8
+        logic.car["glideBonusZ"] = 0.33
+        logic.car["glidecooldown"] = 20
+        logic.car["accelNormal"] = -12
+        logic.car["accelTurbo"] = -15
+        logic.car["turboDur"] = 16
+        logic.car["brakeForce"] = 3.0
+        logic.car["steerAmount"] = 0.015
+        logic.car.actuators["Glide"].linV  = [ 0.0, 0.3, 0.25]
+        ## Wheel/Handling Stats
+        bstat["stiffness"] = 30.0
+        bstat["damping"] = 20.0
+        bstat["Stability"] = 0.1
+        setWheelStats()
+    elif choice == 3:
+        ## Racer - Wild
+        ## Fastest, highest top speed, unstable, longest glide
+        logic.scene.objects["Loop 3_proxy"].setVisible(True)
+        logic.car["activeShape"] = 2
+        logic.car.linVelocityMax = 200
+        logic.car["glideBonusY"] = 1
+        logic.car["accelNormal"] = -15
+        logic.car["accelTurbo"] = -28
+        logic.car["turboDur"] = 12
+        logic.car["brakeForce"] = 10
+        logic.car["glidecooldown"] = 20
+        logic.car["steerAmount"] = 0.25
+        logic.car.actuators["Glide"].linV  = [ 0.0, 0.33, 0.33]
+        ## Wheel/Handling Stats
+        bstat["influence"] = 0.07
+        bstat["stiffness"] = 40.0
+        bstat["damping"] = 20.0
+        bstat["Stability"] = 0.03
+        setWheelStats()
+    elif choice == 4:
+        ## Racer - Tame
+        ## Fast, stable yet nimble, long glide
+        logic.scene.objects["Loop 4_proxy"].setVisible(True)
+        logic.car["activeShape"] = 3
+        logic.car.linVelocityMax = 120
+        logic.car["glideBonusY"] = 1
+        logic.car["glideBonusZ"] = 0.33
+        logic.car["glidecooldown"] = 14
+        logic.car["steerAmount"] = 0.02
+        ## Wheel/Handling Stats
+        bstat["Stability"] = 0.1
+        setWheelStats()
+    elif choice == 5:
+        ## Undecided
+        logic.scene.objects["Loop 1_proxy"].setVisible(True)
+        logic.car["activeShape"] = 0
+    elif choice == 6:
+        ## Undecided
+        logic.scene.objects["Loop 1_proxy"].setVisible(True)
+        logic.car["activeShape"] = 0
+
+
+## Reset max speed after using turbo
+def resetTopSpeed():
+    if logic.car["activeShape"] == 0:
+        logic.car.linVelocityMax = 40
+    elif logic.car["activeShape"] == 1:
+        logic.car.linVelocityMax = 90
+    elif logic.car["activeShape"] == 2:
+        logic.car.linVelocityMax = 200
+    elif logic.car["activeShape"] == 3:
+        logic.car.linVelocityMax = 60
+    elif logic.car["activeShape"] == 4:
+        logic.car.linVelocityMax = 60
+    elif logic.car["activeShape"] == 5:
+        logic.car.linVelocityMax = 60
 
 
 ## called from main car object
@@ -176,19 +296,24 @@ def keyHandler():
     for key in keys:
         ## Forward
         if key[0] == events.WKEY:
-                logic.car["force"]  = -15.0
+            logic.car["force"]  = logic.car["accelNormal"]
         ## Turbo
         if key[0] == events.LEFTSHIFTKEY:
-                logic.car["force"]  = -28.0
+            if logic.car["turboTimer"] < logic.car["turboDur"]:
+                logic.car.linVelocityMax += 5
+                logic.car["force"]  = logic.car["accelTurbo"]
+            else:
+                resetTopSpeed()
         ## Reverse
         elif key[0] == events.SKEY:
-                logic.car["force"]  = 10.0
+            if logic.car["speed"] < 10.0:
+                logic.car["force"]  = 2.0
         ## Right
         elif key[0] == events.DKEY:
-                logic.car["steer"] -= 0.05
+            logic.car["steer"] -= logic.car["steerAmount"]
         ## Left
         elif key[0] == events.AKEY:
-                logic.car["steer"] += 0.05
+            logic.car["steer"] += logic.car["steerAmount"]
         ## Rescue Me!
         elif key[0] == events.RKEY:
             if key[1] == 1:
@@ -203,13 +328,24 @@ def keyHandler():
         ## Brake
         elif key[0] == events.LEFTCTRLKEY:
             if logic.car["speed"] > 2.0:
-                logic.car["force"]  = 15.0
-            if logic.car["speed"] < -2.0:
-                logic.car["force"]  = -15.0
-        ## Maintain momentum when gliding
+                 ## Braking when going forward
+                logic.car["force"]  = logic.car["brakeForce"]
+            if logic.car["speed"] < 0:
+                ## Braking when going backward
+                logic.car["force"]  = -10
+        ## Maintain momentum and lift when gliding
         elif key[0] == events.SPACEKEY:
-            if logic.car["speed"] < 0.2:
-                logic.car.linearVelocity[1] += .6
+            if logic.car["speed"] < 10:
+                logic.car.linearVelocity[1] += logic.car["glideBonusY"]
+                logic.car.linearVelocity[2] += logic.car["glideBonusZ"]
+        elif key[0] == events.PAD1:
+            changeShape(1)
+        elif key[0] == events.PAD2:
+            changeShape(2)
+        elif key[0] == events.PAD3:
+            changeShape(3)
+        elif key[0] == events.PAD4:
+            changeShape(4)
 
 
 ## called from shadow lamp
@@ -218,4 +354,4 @@ def shadow():
         ownpos = [-5.0,0.0,8.0]
         pos = logic.car.worldPosition
         cont.owner.worldPosition = [pos[0]+ownpos[0], pos[1]+ownpos[1], pos[2]+ownpos[2]]
-
+        
