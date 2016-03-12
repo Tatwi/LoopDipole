7a91a46de262e102cda0f8dadec1d70437d13503
Tatwi <rbassettjr@tpot.ca>
2016-03-12 03:06:13 -0500

    Added TV fountain puzzle object that may teleport the player to one of three locations, when the player jumps through the loop antenna. - Added another ribbon connection on the red level.

--- worldTriggers.py
+++ worldTriggers.py
@@ -20,6 +20,7 @@ from bge import logic
 from bge import events
 from bge import render as r
 import math
+from random import randint
 import GameLogic as G
 
 
@@ -40,6 +41,27 @@ def skiJumpEnd():
     logic.car.linVelocityMax = logic.car["myLinVelocityMax"]
     logic.car["limitMaxVelocity"] = True
     logic.car["skiJumping"] = False
+
+
+# Possibly send player to one of 3 locations
+def teleporterTV():
+    rng = randint(1,100)
+    if rng < 33:
+        logic.car.actuators["Explosion"].startSound()
+    else:
+        logic.car.setLinearVelocity([0.0,0.0,0.0],1)
+        if rng > 85:
+            # Purple level, left eye
+            logic.car.position = (98.0, 870.0, 231.0)
+        elif rng > 60:
+            # Blue level
+            logic.car.position = (-995, 961, 206.0)
+        elif rng > 32:
+            # Yellow level, right foot
+            logic.car.position = (-980, -1077, 106.0)
+
+        teleportSound = logic.scene.objects["Controller"].actuators["Load In"]
+        teleportSound.startSound()
 
 
 # Define objects for use in this script
